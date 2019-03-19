61de26edbcb46799bff47cfe4271ccb95e8aac4b
Thalia Velázquez <thalia.velazquez@outlook.com>
2019-03-19 13:07:31 -0600

    FOSUser login 	new file:   app/Resources/views/security/login.html.twig 	new file:   app/Resources/views/security/login_content.html.twig

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+// src/AppBundle/Controller/SecurityController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+   /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login_content.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
